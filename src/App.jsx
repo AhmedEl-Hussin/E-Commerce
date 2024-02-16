09077e80bb2616cbcc3cb1c69c8a6a1a163863f7
“AhmedEl-Hussin” <Ahmad.elhussin03@gmail.com>
2024-02-16 19:24:53 +0200

    implement commponents

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './App.css'
+import NotFound from './Shared/NotFound/NotFound'
+import Home from './Commponents/Home/Home'
+import Brands from './Commponents/Brands/Brands'
+import Cart from './Commponents/Cart/Cart'
+import AllOrders from './Commponents/AllOrders/AllOrders'
+import AuthLayout from './Shared/AuthLayout/AuthLayout';
+import Login from './Commponents/Login/Login'
+import Register from './Commponents/Register/Register'
+import ReqRestPassword from './Commponents/ReqRestPassword/ReqRestPassword'
+import RestPassword from './Commponents/RestPassword/RestPassword'
+import ChangePassword from './Commponents/ChangePassword/ChangePassword'
+import MasterLayout from './Shared/MasterLayout/MasterLayout'
+
+function App() {
+
+  const routes = createBrowserRouter([
+
+    {
+      path : "dashboard",
+      element : <MasterLayout/>,
+      errorElement : <NotFound/>,
+      children : [
+        {index : true , element : <Home/> },
+        {path : "brands" , element : <Brands/> },
+        {path : "cart" , element : <Cart/> },
+        {path : "allorders" , element : <AllOrders/> },
+
+      ]
+    },
+
+    {
+      path: '/',
+      element : <AuthLayout/>,
+      errorElement : <NotFound/>,
+      children : [
+        {index : true , element : <Login/>},
+        {path : "login" , element :<Login/> },
+        {path : "register" , element : <Register/> },
+        {path : "reqRestPassword" , element : <ReqRestPassword/> },
+        {path : "restPassword" , element :<RestPassword/> },
+        {path : "changePassword" , element :<ChangePassword/> },
+      ]
+    }
+
+  ])
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  )
+}
+
+export default App
