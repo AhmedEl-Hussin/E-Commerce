21fc5c353e952e4175721f3972fb1944f5ec3e6e
“AhmedEl-Hussin” <Ahmad.elhussin03@gmail.com>
2024-02-29 23:36:49 +0200

    “Cart-Ui”

--- src/Commponents/Cart/Cart.jsx
+++ src/Commponents/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import man from '../../assets/images/man.jpeg'
 
 export default function Cart() {
     return (
@@ -10,37 +11,49 @@ export default function Cart() {
 
             <div className='cart-caption my-5 p-5'>
                 <h2 className='text-center fw-light puplic-color'> Welcome, Ahmed </h2>
-
+                
                 <div className='mt-3'>
                     <h4>Shop Cart :</h4>
                     <h5 className='fw-light'>Total Cart Price : <span className='puplic-color'>11920 EGP</span> </h5>
                 </div>
 
-                <button className='btn btn-outline-danger mt-2 mb-3'>Clear All</button>
+                <button className='btn btn-outline-danger mt-2 mb-4'>
+                    Clear All
+                    <i className="fa-regular fa-trash-can ms-2"></i>
+                </button>
 
-                <div className='test row'>
+                <div className='cart-line mb-4 row'>
 
-                    <div className='col-md-2'>
-                        <div className='bg-danger'>
-                            <img src="" alt="" />
+                    <div className='col-md-1'>
+                        <div className='cart-img'>
+                            <img className='w-100 h-100' src={man} alt="" />
                         </div>
                     </div>
 
-                    <div className='col-md-8'>
-                        <div className='bg-info'>
-                            <h4>Ahmed</h4>
+                    <div className='col-md-9'>
+                        <div className=''>
+                            <h5 className='fw-light'>Duramo 10 Running Shoes</h5>
+                            <span className='puplic-color'>price : 1999</span> <br />
+                            <button className='btn btn-outline-danger mt-2 mb-3'>
+                                Delete
+                                <i className="fa-regular fa-trash-can ms-2"></i>
+                            </button>
                         </div>
                     </div>
 
                     <div className='col-md-2'>
-                        <div className='bg-warning'>
-                            <h4>Ahmed</h4>
+                        <div className='d-flex count mt-1'>
+                            <button className='btn btn-outline-success'>+</button>
+                            <span className='mx-1 mt-2'>23</span>
+                            <button className='btn btn-outline-success'>-</button>
                         </div>
                     </div>
 
                 </div>
 
             </div>
+
+            
 
 
         </>
--- src/Commponents/Login/Login.jsx
+++ src/Commponents/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
@@ -10,6 +10,19 @@ export default function Login() {
         handleSubmit,
         formState : { errors },
     } = useForm();
+    const [type, setType] = useState("password");
+    const [icon, setIcon] = useState(false);  
+
+
+    const handleToggle = () => {
+        if (type === "password") {
+            setIcon(true);
+            setType("text");
+        } else {
+            setIcon(false);
+            setType("password");
+        }
+    };
 
     return (
         <>
@@ -41,25 +54,44 @@ export default function Login() {
                         </div>
 
         {/* ************************* for input password ************************* */}
-                        <div className='form-group mt-3 mb-2'>
-                            <label htmlFor="">Password :</label>
-                            <input className='py-2 mt-2 form-control fs-5 ' 
-                                type="password" 
-                                {...register("password" , {
-                                required : true,
-                                pattern : /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{6,16}$/
-                                })}
-                            />
+                        <div className='d-flex'>
+                            <div className='w-100 mt-3 mb-2'>
+                                <label htmlFor="">Password :</label>
+                                <input className='py-2 mt-2 form-control fs-5 ' 
+                                    type={type} 
+                                    {...register("password" , {
+                                    required : true,
+                                    pattern : /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{6,16}$/
+                                    })}
+                                />
 
-                            {errors.password && errors.password.type === "required" && (
-                            <span className='text-danger mt-4'>Password is required</span>
-                            )}
+                                {errors.password && errors.password.type === "required" && (
+                                <span className='text-danger mt-4'>Password is required</span>
+                                )}
 
-                            {errors.password && errors.password.type === "pattern" && (
-                            <span className='text-danger mt-4'>Password must include at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 6 characters long</span>
-                            )}
+                                {errors.password && errors.password.type === "pattern" && (
+                                <span className='text-danger mt-4'>Password must include at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 6 characters long</span>
+                                )}
 
+                            </div>
+
+                            <span className="iconTogle-login" onClick={handleToggle} >
+                                {icon ? (
+                                    <i className="fa-solid fa-eye"></i>
+                                ) : (
+                                    <i className="fa-solid fa-eye-slash"></i>
+                                )}
+                            </span>
                         </div>
+                        
+
+                        {/* <span className="" onClick={handleToggle} >
+                            {icon ? (
+                                <i className="fa-solid fa-eye"></i>
+                            ) : (
+                                <i className="fa-solid fa-eye-low-vision"></i>
+                            )}
+                        </span> */}
 
                         <Link className='link' to="/reqRestPassword">Forgot Your Password ?</Link> <br />
                         <button type="submit" className='btn btnSubmit mt-2'> Login </button>
